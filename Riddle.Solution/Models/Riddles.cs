42207814bce42a72fcc982d1fc62cccaeb95bd80
Maxwell Meyer <maxreadswell@gmail.com>
2021-07-12 15:30:58 -0700

    update program.cs to access models
    Co-authored-by: Jenn B <jennifer.bordon@gmail.com>
    Co-authored-by: Erika Debelis <erika.debelis@gmail.com>

--- Riddle.Solution/Models/Riddles.cs
+++ Riddle.Solution/Models/Riddles.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
 
-namespace RiddlesOfTheSphinx
+namespace RiddlesOfTheSphinx.Models
 {
   public class Riddles
   {
-    private static List<Riddle> theRiddles = new List<Riddle>
+    public static List<Riddle> theRiddles = new List<Riddle>
     {
       new Riddle("clock".ToLower(), "What has hands and a face, but can’t hold anything or smile?"),
+      new Riddle("name".ToLower(), "It belongs to you, but your friends use it more. What is it?"),
+      new Riddle("promise".ToLower(), "If you don’t keep me, I’ll break. What am I?"),
+      new Riddle("coin".ToLower(), "I have a tail and a head, but no body. What am I?"),
 
 
     };
--- Riddle.Solution/Program.cs
+++ Riddle.Solution/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using RiddlesOfTheSphinx.Models;
 
 namespace RiddlesOfTheSphinx
 {
@@ -8,7 +10,9 @@ namespace RiddlesOfTheSphinx
     {
       Console.WriteLine("RIDDLES OF THE SPHINX");
       Console.WriteLine("ANSWER IF YOU DARE...");
-
+      List<Riddle> test = Riddles.theRiddles;
+      Console.WriteLine(test[0].Question);
+      Console.WriteLine(test[0].Answer);
     }
   }
 }
